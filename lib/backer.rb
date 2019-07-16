ef69e9aa52f326e5f9155ab84fccf6662465be82
Morgan Stanley <morganpstanley@gmail.com>
2019-07-16 08:26:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,9 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    if project.backers.none(self)
+      project.add_backer(self)
+    end
   end
 
 end
